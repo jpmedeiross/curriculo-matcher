8bfbd54b749a3b2ace0ebaefde4a4b2da6bd1138
João Pedro Medeiros <jpmdrsdev@gmail.com>
2025-07-21 18:32:54 -0300

    wip

--- cmatcher-backend/src/analysis/analysis.module.ts
+++ cmatcher-backend/src/analysis/analysis.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AnalysisController } from './analysis.controller';
 import { AnalysisService } from './analysis.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [AnalysisController],
   providers: [AnalysisService]
 })
